6f614487fb1edc77b1e2288cf95f1c950ca82aab
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-06-12 18:46:52 +0600

    First Commit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,15 @@
+import { Alert, Button, notification } from "antd";
+
 const Dashboard = () => {
-  return <div>Hellow</div>;
+  const openNotification = notification.success({ message: "Success" });
+  return (
+    <>
+      <Alert message="Success Tips" type="success" showIcon />
+      <Button danger type="primary" onClick={() => openNotification}>
+        Click
+      </Button>
+    </>
+  );
 };
 
 export default Dashboard;
